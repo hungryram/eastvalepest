5bdf9b8bf65e4124442f98229bd491eb7c035a29
hungryram <hungryram19@gmail.com>
2023-12-18 22:05:16 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/(site)/components/templates/feature-grid-box.tsx
@@ -0,0 +1,123 @@
+import Styles from "./feature-section.module.css"
+import * as HeroIcons from '@heroicons/react/24/outline';
+import HeaderSection from "./header-section";
+import Link from "next/link";
+import ContentEditor from "../util/content-editor";
+
+interface Props {
+    backgroundStyles: any;
+    columnNumber: number;
+    blocks: any;
+    content: any;
+    textAlign: string;
+    primaryButtonLink: string;
+    primaryButtonText: string;
+    primaryButtonStyle: any;
+    secondaryButtonText: string;
+    secondaryButtonLink: string;
+    secondaryButtonStyle: any;
+    gridBackgroundColor: any;
+    offsetTop: boolean;
+    paddingTop: string;
+    paddingBottom: string;
+}
+
+export default function FeaturedGridBox({
+    backgroundStyles,
+    columnNumber,
+    blocks,
+    content,
+    textAlign,
+    primaryButtonLink,
+    primaryButtonText,
+    primaryButtonStyle,
+    secondaryButtonLink,
+    secondaryButtonText,
+    secondaryButtonStyle,
+    gridBackgroundColor,
+    offsetTop,
+    paddingTop,
+    paddingBottom,
+}: Props) {
+
+    const styles = {
+        paddingTop: paddingTop ?? '5rem',
+        paddingBottom: paddingBottom ?? '5rem',
+    }
+
+    const allStyles = { ...backgroundStyles, ...styles }
+
+    return (
+        <div className={`${offsetTop && '-mt-32 relative'}`} style={allStyles}>
+            <div className={`container`}>
+                {(content || primaryButtonLink || secondaryButtonLink) && (
+                    <HeaderSection
+                        content={content}
+                        textAlign={textAlign}
+                        // PRIMARY
+                        buttonLink={primaryButtonLink}
+                        primaryButtonText={primaryButtonText}
+                        primaryButtonStyle={primaryButtonStyle}
+                        // SECONDARY
+                        secondaryButtonLink={secondaryButtonLink}
+                        secondaryButtonText={secondaryButtonText}
+                        secondaryButtonStyle={secondaryButtonStyle}
+                    />
+                )}
+                <div className={Styles.featureGridWrap}>
+                    <dl className={`grid rounded-sm grid-cols-1 lg:grid-cols-${columnNumber} ${content && 'mt-16'}`} style={{
+                        backgroundColor: gridBackgroundColor
+                    }}>
+                        {blocks?.map((node: any) => {
+
+                            const IconComponent = HeroIcons[node.icon as keyof typeof HeroIcons];
+                            const blockLink: any = node?.blockLinking?.internalLink
+                            const linkUrl =
+                                (blockLink?._type === "pages" && `/${node.blockLinking?.internalLink.slug}`) ||
+                                (blockLink?._type === "blog" && `/blog/${node.blockLinking?.internalLink.slug}`) ||
+                                (blockLink?._type === "legal" && `/legal/${node.blockLinking?.internalLink.slug}`) ||
+                                (blockLink?._type === "services" && `/services/${node.blockLinking?.internalLink.slug}`) ||
+                                (blockLink?._type === "team" && `/team/${node.blockLinking?.internalLink.slug}`) ||
+                                (blockLink?._type === "team" && `/team/${node.blockLinking?.internalLink.slug}`) ||
+                                (node.blockLinking?.externalUrl && `${node.blockLinking?.externalUrl}`);
+                            return (
+                                <div key={node._key} className={`${Styles.featureCardContainer} p-10`}>
+                                    <dt className={`${Styles.featureCard}`} style={{
+                                        color: node?.headingColor?.hex
+                                    }}>
+                                        {IconComponent && (
+                                            <IconComponent className="h-5 w-5 flex-none" style={{
+                                                color: node?.iconColor?.hex
+                                            }} aria-hidden="true" />
+                                        )}
+                                        {node.value}
+                                    </dt>
+                                    <dd className={Styles.featureCardContent}>
+                                        {node?.content &&
+                                            <div style={{
+                                                color: node?.contentColor?.hex
+                                            }}>
+                                                <ContentEditor
+                                                    content={node.content}
+                                                />
+                                            </div>
+                                        }
+                                        {node?.button?.text &&
+                                            <p className="mt-6">
+                                                <Link href={linkUrl ?? '/'} className={`${Styles.featureCardCta}`} aria-label={`Link to ${node?.value}`} style={{
+                                                    color: node?.linkColor?.hex
+                                                }}>
+                                                    {node?.button?.text} <span aria-hidden="true">→</span>
+                                                </Link>
+                                            </p>
+                                        }
+                                    </dd>
+                                </div>
+                            )
+                        })}
+                    </dl>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/app/(site)/components/templates/header-section.tsx
@@ -0,0 +1,81 @@
+import Link from "next/link"
+import ContentEditor from "../util/content-editor"
+
+export default function HeaderSection({
+  content,
+  textAlign,
+  // PRIMARY
+  buttonLink,
+  primaryButtonText,
+  primaryButtonStyle,
+  // SECONDARY
+  secondaryButtonLink,
+  secondaryButtonText,
+  secondaryButtonStyle
+}: any) {
+
+  const primaryButtonLinking =
+    (buttonLink?.internalLink?._type === "pages" && `/${buttonLink?.internalLink.slug}`) ||
+    (buttonLink?.internalLink?._type === "blog" && `/blog/${buttonLink?.internalLink.slug}`) ||
+    (buttonLink?.internalLink?._type === "legal" && `/legal/${buttonLink?.internalLink.slug}`) ||
+    (buttonLink?.internalLink?._type === "services" && `/services/${buttonLink?.internalLink.slug}`) ||
+    (buttonLink?.internalLink?._type === "team" && `/team/${buttonLink?.internalLink.slug}`) ||
+    (buttonLink?.internalLink?._type === "homeDesign" && `/`) ||
+    (buttonLink?.externalUrl && `${buttonLink?.externalUrl}`)
+
+  const secondaryButtonLinking =
+    (secondaryButtonLink?.internalLink?._type === "pages" && `/${secondaryButtonLink?.internalLink.slug}`) ||
+    (secondaryButtonLink?.internalLink?._type === "blog" && `/blog/${secondaryButtonLink?.internalLink.slug}`) ||
+    (secondaryButtonLink?.internalLink?._type === "legal" && `/legal/${secondaryButtonLink?.internalLink.slug}`) ||
+    (secondaryButtonLink?.internalLink?._type === "services" && `/services/${secondaryButtonLink?.internalLink.slug}`) ||
+    (secondaryButtonLink?.internalLink?._type === "team" && `/team/${secondaryButtonLink?.internalLink.slug}`) ||
+    (secondaryButtonLink?.internalLink?._type === "homeDesign" && `/`) ||
+    (secondaryButtonLink?.externalUrl && `${secondaryButtonLink?.externalUrl}`)
+
+  return (
+    <>
+      {primaryButtonLinking || secondaryButtonLinking ? (
+        <div className={`content 
+        ${textAlign === 'left' && 'text-left max-w-4xl'}
+        ${textAlign === 'center' && 'mx-auto text-center justify-center max-w-4xl'}
+        ${textAlign === 'right' && 'text-right justify-end'}
+        `}>
+          <ContentEditor content={content} />
+          <div className={`mt-10 flex items-center gap-x-6 ${textAlign === 'left' && 'text-left max-w-4xl'} ${textAlign === 'center' && 'mx-auto text-center justify-center max-w-4xl'} ${textAlign === 'right' && 'text-right justify-end'}`}>
+          {primaryButtonLinking && (
+              <>
+                {buttonLink?.externalUrl ?
+                  <a href={primaryButtonLinking} className="primary-button" style={primaryButtonStyle}>{primaryButtonText}</a>
+                  :
+                  <Link href={primaryButtonLinking} className="primary-button" style={primaryButtonStyle}>
+                    {primaryButtonText}
+                  </Link>
+                }
+              </>
+            )}
+            {secondaryButtonLinking && (
+              <>
+                {secondaryButtonLink?.externalUrl ?
+                  <a href={secondaryButtonLinking} className="secondary-button" style={secondaryButtonStyle}>{secondaryButtonText}</a>
+                  :
+                  <Link href={secondaryButtonLinking} className="secondary-button" style={secondaryButtonStyle}>
+                    {secondaryButtonText} <span aria-hidden="true">→</span>
+                  </Link>
+                }
+              </>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className={`content 
+        ${textAlign === 'left' && 'text-left max-w-4xl'}
+        ${textAlign === 'center' && 'mx-auto text-center justify-center max-w-4xl'}
+        ${textAlign === 'right' && 'text-right justify-end'}
+        `}>
+          <ContentEditor content={content} />
+        </div>
+      )}
+
+    </>
+  )
+}
